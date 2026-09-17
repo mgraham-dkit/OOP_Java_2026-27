0dcc9e75d0f0360853b5ef57d2480b3e6124acdb
michelle <michelle.graham@dkit.ie>
2026-09-17 16:01:42 +0100

    solutions: week01 (Revision) - Exercise 1.2 - Basic IO Work - Calculating the tiles required to cover an area (height & width-based calculation)

---
+++ src/main/java/solutions/week01/revision/CalcTileQuantity_Version2.java
@@ -0,0 +1,33 @@
+package solutions.week01.revision;
+
+import java.util.Scanner;
+
+public class CalcTileQuantity_Version2 {
+    public static void main(String [] args) {
+        // Create access to console/keyboard
+        Scanner input = new Scanner(System.in);
+
+        // Inform user what's going on
+        System.out.println("Tile Area Calculator");
+        System.out.println("Note: All tiles measure 3 * 3");
+        int edgeLength = 3;
+
+        // Take in required information
+        System.out.println("Enter your space dimensions: ");
+        System.out.print("\tHeight: ");
+        int height = input.nextInt();
+        System.out.print("\tWidth: ");
+        int width = input.nextInt();
+
+        // Calculate how many tiles are needed for the height
+        // If we have a partial tile in the calculation, Java will automatically
+        // round downwards. However, we need to round up in this case (or we won't have enough tiles)
+        // Math.ceil() rounds upwards, but it works on doubles
+        // We need to cast to use the double version of the value,
+        // then convert back to ints for the final answer
+        int heightRequired = (int) Math.ceil((double)height/edgeLength);
+        int widthRequired = width/edgeLength;
+        System.out.println("You will need " + (heightRequired*widthRequired) + " tiles to cover your space (Surface area " +
+                "needed: " + (height*width) + ").");
+    }
+}
